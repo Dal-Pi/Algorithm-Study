a217285d8fa35f19294701acb96fb0301708e280
hyewon1988 <hyewon1988@gmail.com>
2015-12-13 11:09:41 +0900

    [Hyewon] WILDCARD (DP)

--- Hyewon/WILDCARD.c
+++ Hyewon/WILDCARD.c
@@ -8,6 +8,7 @@ int R;
 char W[102];
 char S[51][102];
 char ANSWERS[51][102];
+int CACHE[51][101][101];
 
 int comparisonFuncitonString(const void * a, const void * b)
 {
@@ -16,35 +17,45 @@ int comparisonFuncitonString(const void * a, const void * b)
 
 int check(int idx, int w, int s){
 
-	while (W[w] != '\0' && S[idx][s] != '\0' && (W[w] == '?' || W[w] == S[idx][s])){
-		++w;
-		++s;
+	if (CACHE[idx][w][s] != -1){
+		return CACHE[idx][w][s];
 	}
 
-	if (W[w] == '\0'){
-		if (S[idx][s] == '\0'){
+	while (W[w] != '\0' && S[idx][s] != '\0' && (W[w] == '?' || W[w] == S[idx][s])){
+		if (check(idx, w + 1, s + 1) == 1){
+			CACHE[idx][w][s] = 1;
 			return 1;
 		}
 		else{
+			CACHE[idx][w][s] = 0;
+			return 0;
+		}
+	}
+
+
+	if (W[w] == '\0'){
+		if (S[idx][s] == '\0'){
+			CACHE[idx][w][s] = 1;
+			return 1;
+		}
+		else{
+			CACHE[idx][w][s] = 0;
 			return 0;
 		}
 	}
 	
 	if (W[w] == '*'){
-		int skip = 0;
-		while (1){
-			if (check(idx, w + 1, s + skip) == 1){
-				return 1;
-			}
-			if (S[idx][s + skip] == '\0'){
-				break;
-			}
-			else{
-				skip++;
-			}
+		if (check(idx, w+1, s) == 1){
+			CACHE[idx][w][s] = 1;
+			return 1;
+		}
+		if (S[idx][s] != '\0' && check(idx, w, s + 1) == 1){
+			CACHE[idx][w][s] = 1;
+			return 1;
 		}
 	}
 
+	CACHE[idx][w][s] = 0;
 	return 0;
 }
 
@@ -55,6 +66,14 @@ int main(){
 
 	while (T--){
 		R = 0;
+		int i, j, k;
+		for (i = 0; i < 51; i++){
+			for (j = 0; j < 101; j++){
+				for (k = 0; k < 101; k++){
+					CACHE[i][j][k] = -1;
+				}
+			}
+		}
 		scanf("%s", &W);
 		scanf("%d", &N);
 
